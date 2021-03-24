e8c1d01cd855a3945ab1927acb33b419480af9b5
Jack Kavanagh <me@jack-kavanagh.co.uk>
2021-03-24 16:40:37 +0000

    test

---
+++ out/production/CreatorGames/src/jackgnuk/creatorgames/listener.java
@@ -0,0 +1,113 @@
+package jackgnuk.creatorgames;
+
+import me.wazup.partygames.PartyGames;
+import me.wazup.partygames.PartyGamesAPI;
+import me.wazup.partygames.PlayerData;
+import me.wazup.partygames.minigames.AnimalSlaughter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.io.File;
+import java.util.UUID;
+
+public class listener implements Listener {
+
+    // On PlayerJoin
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(ChatColor.GREEN + "+ " + ChatColor.GRAY + event.getPlayer().getName() + " Has Joined.");
+    }
+    // On PlayerLeave
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        event.setQuitMessage(ChatColor.RED + "- " + ChatColor.GRAY + event.getPlayer().getName() + " Has Left.");
+    }
+    @EventHandler
+    public void getCoin(EntityDeathEvent event) {
+
+        LivingEntity entity = event.getEntity();
+
+        PartyGamesAPI api = PartyGames.api;
+        Player player = event.getEntity().getKiller();
+        Player p = Bukkit.getPlayer(player.getName());
+        PlayerData data = api.getPlayerData(p);
+
+
+        if (entity.getType() == EntityType.BAT) {
+            if (player != null) {
+                entity.getKiller().sendMessage(ChatColor.YELLOW + "+1  Point");
+                entity.getKiller().playSound(player.getLocation(), Sound.BLOCK_BELL_USE, 1F, 1F);
+                //Gives killer 10 Coins from the API of PartyGames
+                data.addCoins(p, 10);
+                data.saveStats = true;
+
+            } else {
+                return;
+            }
+        }
+    }
+
+
+//    // On DeathEvent Handler
+//    @EventHandler
+//    public void onDeath(EntityDeathEvent e) {
+//        PartyGamesAPI api = PartyGames.api;
+//        Player player = (Player) e.getEntity();
+//        Player killer = player.getKiller();
+//        // killer to be a player name
+//        Player p = Bukkit.getPlayer(killer.getName());
+//        PlayerData data = api.getPlayerData(p);
+//
+//        // Checks player was killed by Player
+//        if (player != null && killer != null) {
+//
+//            //Gives killer 10 Coins from the API of PartyGames
+//            data.addCoins(killer, 10);
+//            data.saveStats = true;
+//        }
+//    }
+
+    // Player onDeath set Messages
+    @EventHandler
+    public  void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        Player killer = player.getKiller();
+        // Check if player was killed by a player not entity.
+        if (player != null && killer != null) {
+            //Sets DeathScreen/Chat message to Nothing
+            event.setDeathMessage(ChatColor.RED + "" + ChatColor.STRIKETHROUGH + "");
+
+            //Broadcast Message of Death in Chat
+            Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.STRIKETHROUGH + "-------------------------------");
+            Bukkit.broadcastMessage(ChatColor.GRAY + "");
+            Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " was killed by " + killer.getName());
+            Bukkit.broadcastMessage(ChatColor.GRAY + "");
+            Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.STRIKETHROUGH + "-------------------------------");
+        }
+    }
+
+   // @EventHandler
+   // public void onKill(EntityDeathEvent d) {
+   //     Player player = (Player) d.getEntity();
+
+   //     if (event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent) {
+   //         EntityDamageByEntityEvent nEvent = (EntityDamageByEntityEvent) d.getEntity().getLastDamageCause();
+
+   //         if ((nEvent.getDamager() instanceof Bat)) {
+                // this wont work atm
+   //         } else {
+   //             return;
+   //         }
+   //     }
+   // }
+}
